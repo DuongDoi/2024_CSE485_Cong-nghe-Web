c2b8438bf2d547e0ea74d786a96230e00a612489
NgocDiu <146963679+NgocDiu@users.noreply.github.com>
2024-02-20 23:52:38 +0700

    update Diu

--- project12/index.php
+++ project12/index.php
@@ -1,36 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bai Tap 2</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+<html>
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
- <ul class="ulnav"> 
-        
-        <?php
-        
-        $navItems = [
-            "GIỚI THIỆU",
-            "TIN TỨC & THÔNG BÁO",
-            "TUYỂN SINH",
-            "ĐÀO TẠO",
-            "NGHIÊN CỨU",
-            "ĐỐI NGOẠI",
-            "VĂN BẢN",
-            "SINH VIÊN",
-            "LIÊN HỆ"
-        ];
-        echo"<li class='linav'><a class='fa fa-home' href='#'></a></li>";
-        foreach ($navItems as $item) {
-            echo "<li class='linav'><a href='#'>$item</a></li>";
-        }
-        ?>
-    </ul>  
+    <head>
+    </head>
+    <body>
+        <ul>
+            <?php
+                $navItems = [
+                "GIỚI THIỆU",
+                "TIN TỨC & THÔNG BÁO",
+                "TUYỂN SINH",
+                "ĐÀO TẠO",
+                "NGHIÊN CỨU",
+                "ĐỐI NGOẠI",
+                "VĂN BẢN",
+                "SINH VIÊN",
+                "LIÊN HỆ"];
 
+                echo "<li><a href = '#' class='fa fa-home' style='font-size:24px'></a></li>";
 
-</body>
+                foreach ($navItems as $item) {
+                    echo "<li><a href='#'>$item</a></li>";
+                }
+            ?>
+        </ul>
+    </body>
 </html>
