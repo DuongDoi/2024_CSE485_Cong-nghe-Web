cd8bb5afb93d5b6d723348e9d3f9f311fc398348
NgocDiu <146963679+NgocDiu@users.noreply.github.com>
2024-02-26 08:15:33 +0700

    Commit message

--- project14/index.php
+++ project14/index.php
@@ -1,33 +1,145 @@
-<?php
-$navItems = [
- "GIỚI THIỆU",
- "TIN TỨC & THÔNG BÁO",
- "TUYỂN SINH",
- "ĐÀO TẠO",
- "NGHIÊN CỨU",
- "ĐỐI NGOẠI",
- "VĂN BẢN",
- "SINH VIÊN",
- "LIÊN HỆ"
-];
-echo '<nav><ul>';
-foreach ($navItems as $item) {
- echo "<li>$item</li>";
-}
-echo '</ul></nav>';
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bai Tap 2</title>
+    <title>Bai Tap 3</title>
+    <link rel = "stylesheet" href = "css/css.css">
 </head>
 <body>
-    <div name = "head">
-        <div></div>
-        <div></div>
-        <div></div>
+    <div>
+        <h4>Basic Info</h4>
+        <div class = "baInfo">
+            <form action = "" method = "$_POST">
+                <table>
+                    <tr>
+                        <th>Employee ID</th>
+                        <td><input type="text" name="emID" value=""></td>
+                    </tr>
+                    <tr>
+                        <th>Last Name</th>
+                        <td><input type="text" name="lName" value=""></td>
+                    </tr>
+                    <tr>
+                        <th>First Name</th>
+                        <td><input type="text" name="fName" value=""></td>
+                    </tr>
+                    <tr>
+                        <th>Gender</th>
+                        <td><input type="radio" name="gender" value="Male">Male</td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td><input type="radio" name="gender" value="Female">Female</td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td><input type="radio" name="gender" value="XXX">XXX</td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td><input type="radio" name="gender" value="ZZZ">ZZZ</td>
+                    </tr>
+                    <tr>
+                        <th>Title</th>
+                        <td><input type="text" name="tit" value=""></td>
+                    </tr>
+                    <tr>
+                        <th>Suffix</th>
+                        <td><input type="text" name="suf" value=""></td>
+                    </tr>
+                    <tr>
+                        <th>BirthDate</th>
+                        <td><input type="text" name="bDate" value="">(?)</td>
+                    </tr>
+                    <tr>
+                        <th>HireDate</th>
+                        <td><input type="text" name="hDate" value=""></td>
+                    </tr>
+                    <tr>
+                        <th>SSN # (if applicable)</th>
+                        <td><input type="text" name="SSN" value=""></td>
+                    </tr>
+                    <tr>
+                        <th>Reports To</th>
+                        <td>
+                            <?php
+                                $reportsTo = array(
+                                    "Buchanan"
+                                );
+                            ?>
+                            <input type="number" name="rep" value="">
+                            
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+    <div>
+        <h4>Contact Info</h4>
+        <div class = "conInfo">
+            <form action = "" method = "$_POST">
+                <table>
+                    <tr>
+                        <th>Email</th>
+                        <td><input type="text" name="email" value=""></td>
+                    </tr>
+                    <tr>
+                        <th>Address</th>
+                        <td><input type="text" name="address" value=""></td>
+                    </tr>
+                    <tr>
+                        <th>City</th>
+                        <td><input type="text" name="city" value=""></td>
+                    </tr>
+                    <tr>
+                        <th>Region</th>
+                        <td><input type="text" name="region" value=""></td>
+                    </tr>
+                    <tr>
+                        <th>Postal Code</th>
+                        <td><input type="text" name="poCode" value=""></td>
+                    </tr>
+                    <tr>
+                        <th>Country</th>
+                        <td>
+                        <?php
+                            $countries = array (
+                            "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", 
+                            "Antigua & Barbuda",
+                            "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", 
+                            "Bahamas", "Bahrain",
+                            "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", 
+                            "Bermuda", "Bhutan",
+                            "Bolivia", "Bosnia & Herzegovina", "Botswana", "Brazil", "British Virgin 
+                            Islands", "Brunei",
+                            "Bulgaria", "Burkina Faso", "Burundi", "Vietnam"
+                            );
+                            
+                        ?>
+                            <select id="cars" name="cars">
+                                <option value="volvo">Volvo</option>
+                                <option value="saab">Saab</option>
+                                <option value="fiat">Fiat</option>
+                                <option value="audi">Audi</option>
+                            </select>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+    <div>
+        <h4>Optional Info</h4>
+        <div class = "opInfo">
+            <form action = "" method = "$_POST">
+                
+            </form>
+        </div>
+    </div>
+    <div class = "footer">
+
     </div>
 </body>
 </html>
--- project22/index.php
+++ project22/index.php
@@ -1,4 +1,13 @@
-
+<?php
+    $user = [
+        "id" => 1,
+        "name" => "Antonio Fenice",
+        "email" => "afenice@canamerarealty.com",
+        "phNum" => "343-459-4350",
+        "comName" => "CanAmera Realty",
+        "avatar" => "uploads/default_avatar.png" // Initial avatar URL
+       ];
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,6 +17,45 @@
     <title>Document</title>
 </head>
 <body>
+    <div>
+        <h2>Account Setttings</h2>
+    </div>
+
+    <div class="content">
+        <div class="menu">
+            <?php
+                        $navItems = [
+                            "Profile",
+                            "Password",
+                            "Integrations",
+                            "Billing"
+                        ];
+
+                        foreach ($navItems as $item) {
+                            echo "<li><a href='#'>$item</a></li>";
+                        }
+            ?>
+        </div>
+        <div class="pro">
+            <form action="update_profile.php" method="post" enctype="multipart/form-data">
+                <button type="submit">Change My Avatar</button><br>
+                <label for="name">Full Name</label><br>
+                <input type="text" name="name" value="<?php echo $user['name']; ?>" 
+                required>
+                <br>
+                <label for="email">Email</label><br>
+                <input type="email" name="email" value="<?php echo $user['email']; ?>" 
+                disabled>
+                <br>
+                <label for="phNum">Phone Number</label><br>
+                <input type="text" name="phNum" value="<?php echo $user['phNum']; ?>">
+                <br>
+                <label for="comName">Company Name</label><br>
+                <input type="text" name="comName" value="<?php echo $user['comName']; ?>">
+            </form>
+        </div>
+    </div>
     
+
 </body>
 </html>
