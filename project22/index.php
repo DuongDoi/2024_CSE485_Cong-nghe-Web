7f337f97a6bba90c40db5b8cabf20ee772c988c3
NgocDiu <146963679+NgocDiu@users.noreply.github.com>
2024-02-27 23:52:33 +0700

    upload

--- project22/index.php
+++ project22/index.php
@@ -5,25 +5,26 @@
         "email" => "afenice@canamerarealty.com",
         "phNum" => "343-459-4350",
         "comName" => "CanAmera Realty",
-        "avatar" => "uploads/default_avatar.png" // Initial avatar URL
+        "avatar" => "assets/image/avatar1.jpeg" // Initial avatar URL
        ];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Account Settings</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <div>
-        <h2>Account Setttings</h2>
-    </div>
-
-    <div class="content">
-        <div class="menu">
-            <?php
+    <div class = "main">
+        <div class = "header">
+            <h1>Account Setting</h1>
+        </div>
+        <div class = "content">
+                <ul>
+                    <?php
                         $navItems = [
                             "Profile",
                             "Password",
@@ -32,30 +33,36 @@
                         ];
 
                         foreach ($navItems as $item) {
-                            echo "<li><a href='#'>$item</a></li>";
+                            echo "<li><a href='#' >$item</a></li>";
                         }
-            ?>
-        </div>
-        <div class="pro">
-            <form action="update_profile.php" method="post" enctype="multipart/form-data">
-                <button type="submit">Change My Avatar</button><br>
-                <label for="name">Full Name</label><br>
-                <input type="text" name="name" value="<?php echo $user['name']; ?>" 
-                required>
-                <br>
-                <label for="email">Email</label><br>
-                <input type="email" name="email" value="<?php echo $user['email']; ?>" 
-                disabled>
-                <br>
-                <label for="phNum">Phone Number</label><br>
-                <input type="text" name="phNum" value="<?php echo $user['phNum']; ?>">
-                <br>
-                <label for="comName">Company Name</label><br>
-                <input type="text" name="comName" value="<?php echo $user['comName']; ?>">
-            </form>
+                    ?>
+                </ul>
+
+            <div class = "pro" id = "Profilediv">
+                <form action="upload_profile.php" method="post" enctype="multipart/form-data">
+                    <div class="imgavt">
+                        <?php
+                            echo '<img src="' . $user['avatar'] . '" alt="">';
+                        ?>
+                    </div><br>
+                    <input type="file" class="chooseavt" accept="image/*">
+                    <br><br>
+                    <label for="name">Full Name</label><br><br>
+                    <input class="styInput" type="text" name="name" value="<?php echo $user['name']; ?>" required>
+                    <br><br>
+                    <label for="email">Email</label><br><br>
+                    <input class="styInput" type="email" name="email" value="<?php echo $user['email']; ?>" disabled>
+                    <br><br>
+                    <label for="phNum">Phone Number</label><br><br>
+                    <input class="styInput" type="text" name="phNum" value="<?php echo $user['phNum']; ?>">
+                    <br><br>
+                    <label for="comName">Company Name</label><br><br>
+                    <input class="styInput" type="text" name="comName" value="<?php echo $user['comName']; ?>">
+                    <br><br>
+                    <button type="submit" class="btnUpdate">Update Profile</button>
+                </form>
+            </div>
         </div>
     </div>
-    
-
 </body>
 </html>
